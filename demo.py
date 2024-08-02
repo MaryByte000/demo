e3a69e231fea9fe389f2db97069bfac677d66794
MaryByte000 <marykristinecaneda@gmail.com>
2024-08-02 22:06:49 +0800

    demo

---
+++ demo.py
@@ -0,0 +1,47 @@
+print("Hello, World")
+
+hw_1_score = 10
+hw_2_score = 15
+hw_3_score = 10
+sum = hw_1_score + hw_2_score + hw_3_score
+total = sum*3/100
+
+print("the sum is", sum)
+print("the total is", total)
+
+first_name_1 = "Mary"
+first_name_2 = "Kurt"
+first_name_3 = "Mel"
+first_name_4 = "Nat"
+first_name_5 = "Jayrhel"
+people_age = {
+    "Mary": 21,
+    "Kurt": 21,
+    "Mel": 20,
+    "Nat": 20,
+    "Jayhrel": 18
+}
+
+dict ={}
+dict ["one"] = "This is Mary."
+dict ["two"] = "This is Kurt."
+
+print (dict ["one"])
+print (dict ["two"])
+
+
+a = 27
+b = 11
+c = 12
+d = 5
+e = 'n'
+set; e = 0
+
+e = (a+b)*c/d
+print("value of (a+b)*c/d is", e)
+e =((a+b)*c)/d
+print("value of ((a+b)*c)/d", e)
+e = (a+b)*c/d;
+print("value of (a+b)*c/d is", e)
+e = a+(b*c)/d;
+print("value of a+(b*c)/d", e)
